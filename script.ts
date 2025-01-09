969341d1f8abd0bd31c6ee6eecc121d8ef1698ea
Trevor Pierce <trevor_pierce@brown.edu>
2025-01-09 14:11:22 -0600

    Setup script.ts to create seed data

--- script.ts
+++ script.ts
@@ -2,14 +2,32 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.user.create({
+async function clearSeedData() {
+  await prisma.post.deleteMany({});
+  await prisma.user.deleteMany({});
+}
+
+async function createSeedData() {
+  await prisma.user.create({
     data: {
-      name: "Alice",
-      email: "alice@prisma.io",
+      name: "Trevor",
+      email: "trevor@test.edu",
+      posts: {
+        create: {
+          title: "Hello World",
+          content: "Just a test post",
+          published: true,
+        },
+      },
     },
   });
-  console.log(user);
+}
+
+async function main() {
+  await clearSeedData();
+  await createSeedData();
+
+  console.info("Seed data populated");
 }
 
 main()
