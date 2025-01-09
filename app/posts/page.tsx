c93615c4120b006f9798563eca01618e11cb8232
Trevor Pierce <trevor_pierce@brown.edu>
2025-01-09 15:41:35 -0600

    Display more info for a post on the posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,20 @@
 import prisma from "@/lib/prisma";
 
 export default async function Page() {
-  const posts = await prisma.post.findMany({});
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+    },
+  });
 
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
+        <li key={post.id}>
+          <h1>{post.title}</h1>
+          <h2>by {post.author.name}</h2>
+          <p>{post.content}</p>
+        </li>
       ))}
     </ul>
   );
