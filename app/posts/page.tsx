9e92e6c9b35c2bc6d461de278b9fc1648d7ed37c
Trevor Pierce <trevor_pierce@brown.edu>
2025-01-10 11:39:39 -0600

    Change display of posts to be a grid

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -8,25 +8,27 @@ export default async function Page() {
   });
 
   return (
-    <div className="flex flex-col items-center gap-5 p-5">
-      {posts
-        .filter((post) => post.published)
-        .map((post) => (
-          <article
-            className="bg-zinc-50 border-2 rounded border-solid border-zinc-200 w-full p-2 md:w-1/3"
-            key={post.id}
-          >
-            <h2 className="text-lg">{post.title}</h2>
-            <h3 className="text-xs">
-              {post.createdAt.toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
-            </h3>
-            <hr className="border-zinc-400" />
-          </article>
-        ))}
+    <div className="flex flex-col items-center p-5">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-4 w-full md:w-1/3">
+        {posts
+          .filter((post) => post.published)
+          .map((post) => (
+            <article
+              className="bg-zinc-50 border-2 rounded border-solid border-zinc-200 p-2"
+              key={post.id}
+            >
+              <h2 className="text-xl">{post.title}</h2>
+              <h3 className="text-xs">
+                {post.createdAt.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </h3>
+              <hr className="border-zinc-400" />
+            </article>
+          ))}
+      </div>
     </div>
   );
 }
