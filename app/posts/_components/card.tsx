703c90e20350dac30abe0f24f759bbacaa60c07c
Trevor Pierce <trevor_pierce@brown.edu>
2025-01-10 12:06:12 -0600

    Reduce border outline for cards and add drop-shadow

--- app/posts/_components/card.tsx
+++ app/posts/_components/card.tsx
@@ -1,9 +1,9 @@
-import { Post } from "@prisma/client";
+import type { Post } from "@prisma/client";
 
 export default function Card({ post }: { post: Post }) {
   return (
     <article
-      className="bg-zinc-50 border-2 rounded border-solid border-zinc-200 p-2"
+      className="bg-zinc-100 border-1 rounded shadow-xl p-2"
       key={post.id}
     >
       <h2 className="text-xl">{post.title}</h2>
