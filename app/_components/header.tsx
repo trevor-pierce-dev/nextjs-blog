fd5f6d9ba68338a8873f692cb450d6e0e219b3d7
Trevor Pierce <trevor_pierce@brown.edu>
2025-01-10 14:24:13 -0600

    Clean up design of header and add links

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="inline-block w-full md:w-1/2 p-3">
-      <div className="float-start">Just a Blog</div>
-      <div className="float-end">Create New Post +</div>
+    <header className="inline-block w-full md:w-1/2 pb-3 px-1">
+      <div className="float-start font-bold text-xl p-2">
+        <Link href="/">Just a Blog</Link>
+      </div>
+      <div className="float-end border bg-zinc-100 border-1 rounded border-zinc-300 p-2">
+        <Link href="/posts/new">Create New Post +</Link>
+      </div>
     </header>
   );
 }
