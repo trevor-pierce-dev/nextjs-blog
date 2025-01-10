733455e64a9f665109d73cbb0b9c418271ef17bb
Trevor Pierce <trevor_pierce@brown.edu>
2025-01-10 13:04:23 -0600

    Cleanup formatting of single post page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -11,5 +11,20 @@ export default async function Page({
       id: Number(id),
     },
   });
-  return <div>My Post: {post.title}</div>;
+  return (
+    <article
+      className="bg-zinc-100 border-1 rounded shadow-xl p-2 w-full md:w-1/3"
+      key={post.id}
+    >
+      <h2 className="text-xl">{post.title}</h2>
+      <h3 className="text-xs">
+        {post.createdAt.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </h3>
+      <p>{post.content}</p>
+    </article>
+  );
 }
